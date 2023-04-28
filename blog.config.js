c9ed413d9092ca70d051e817ac7de4b495dbe864
JediXL <40334363+JediXL@users.noreply.github.com>
2023-04-28 13:30:23 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,9 +1,9 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
-  link: 'https://nobelium.vercel.app',
-  description: 'This gonna be an awesome website.',
+  title: 'Fogland',
+  author: 'JediXL',
+  email: 'Xiangyangan@gmail.com',
+  link: 'https://fogland-jxl.vercel.app',
+  description: 'Welcome to Fogland.',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],

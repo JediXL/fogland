272780d3ad469543f19f41ed7f39e3096544167d
JediXL <40334363+JediXL@users.noreply.github.com>
2024-09-01 20:42:11 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,10 +1,10 @@
 const BLOG = {
   title: '浓雾荒原',
-  author: 'JediXL',
+  author: 'JD',
   email: 'liujadi@gmail.com',
   link: 'https://fogland-jxl.vercel.app',
   description: 'Welcome to Fogland.',
-  lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
+  lang: 'zh-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
